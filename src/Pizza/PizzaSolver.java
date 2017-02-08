61586773521885ed87cbcf342060d49cf9107138
carmocca <carlossmocholi@hotmail.com>
2017-02-08 17:47:20 +0100

    Renamed some methods

--- src/Pizza/PizzaSolver.java
+++ src/Pizza/PizzaSolver.java
@@ -1,7 +1,5 @@
 package Pizza;
 
-import java.util.List;
-
 public class PizzaSolver {
 
   private Pizza pizza;
@@ -13,9 +11,11 @@ public class PizzaSolver {
   public void generateSlices(int min, int max) {
     for (int i = 0; i < pizza.getCellMatrix().length; i++) {
       for (int j = 0; j < pizza.getCellMatrix()[0].length; j++) {
-        if (pizza.hasSliceIn(i,j))
+        if (pizza.belongsToSlice(i,j)) {
           continue;
-        generateSlice(pizza.getCellMatrix()[i][j], min, max);
+        } else {
+          generateSlice(pizza.getCellMatrix()[i][j], min, max);
+        }
       }
     }
   }
