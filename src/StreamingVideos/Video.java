866742013abbc7fe004b5ff5658393106987062a
Jose Llorens Ripolles <jose.llorens.ripolles@gmail.com>
2017-02-23 19:02:44 +0100

    Added Video class

---
+++ src/StreamingVideos/Video.java
@@ -0,0 +1,19 @@
+package StreamingVideos;
+
+public class Video {
+  int id, size;
+
+  public Video(int id, int size) {
+    this.id = id;
+    this.size = size;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+}
