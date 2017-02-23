0e036e557e9c738cfc1f302ca0f372d1a069c503
Jose Llorens Ripolles <jose.llorens.ripolles@gmail.com>
2017-02-23 19:00:58 +0100

    Created connection and endpoint class

--- src/StreamingVideos/Connection.java
+++ src/StreamingVideos/Connection.java
@@ -1,4 +1,19 @@
 package StreamingVideos;
 
 public class Connection {
+  CacheServer cacheServer;
+  int latency;
+
+  public Connection(CacheServer cacheServer, int latency) {
+    this.cacheServer = cacheServer;
+    this.latency = latency;
+  }
+
+  public CacheServer getCacheServer() {
+    return cacheServer;
+  }
+
+  public int getLatency() {
+    return latency;
+  }
 }
--- src/StreamingVideos/Endpoint.java
+++ src/StreamingVideos/Endpoint.java
@@ -1,4 +1,20 @@
 package StreamingVideos;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Endpoint {
+
+  int id, latency;
+  List<Connection> cacheServerConnections;
+
+  public Endpoint(int id, int latency) {
+    this.id = id;
+    this.latency = latency;
+    this.cacheServerConnections = new LinkedList<>();
+  }
+
+  public void addCacheServer(CacheServer c,int latency){
+    cacheServerConnections.add(new Connection(c,latency));
+  }
 }
